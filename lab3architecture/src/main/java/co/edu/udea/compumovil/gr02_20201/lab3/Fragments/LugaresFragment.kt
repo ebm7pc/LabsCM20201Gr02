6f49be5949909a4dd6b924cfd86fe7e85eb81bec
ebm7pc <ebm7pc@gmail.com>
2020-11-23 19:31:29 -0500

    *Falta agregar retrofit *Problema con ViewModel solucionado -En ocasiones sigue saliendo un mensaje de error en el logcat referente a que no puede instanciar la clase viewModelFactory

--- lab3architecture/src/main/java/co/edu/udea/compumovil/gr02_20201/lab3/Fragments/LugaresFragment.kt
+++ lab3architecture/src/main/java/co/edu/udea/compumovil/gr02_20201/lab3/Fragments/LugaresFragment.kt
@@ -7,30 +7,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.Toast
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
-import co.edu.udea.compumovil.gr02_20201.lab3.Adaptadores.AdapterLugares
+import co.edu.udea.compumovil.gr02_20201.lab3.Adaptadores.OnPlaceItemClickListener
+import co.edu.udea.compumovil.gr02_20201.lab3.Adaptadores.PlaceAdapter
 import co.edu.udea.compumovil.gr02_20201.lab3.Interfaces.iComunicaFragments
 import co.edu.udea.compumovil.gr02_20201.lab3.Persistencia.Dao.PlaceDao
 import co.edu.udea.compumovil.gr02_20201.lab3.Persistencia.DataBase.LabTresDB
 import co.edu.udea.compumovil.gr02_20201.lab3.Persistencia.Entidades.Place
 import co.edu.udea.compumovil.gr02_20201.lab3.R
+import co.edu.udea.compumovil.gr02_20201.lab3.Repositorio.PlaceRepository
 import co.edu.udea.compumovil.gr02_20201.lab3.ViewModel.PlaceViewModel
+import co.edu.udea.compumovil.gr02_20201.lab3.ViewModel.PlaceViewModelFactory
 import kotlinx.android.synthetic.main.fragment_lugares.*
 import kotlin.collections.ArrayList
 
-class LugaresFragment : Fragment() {
+class LugaresFragment : Fragment(), OnPlaceItemClickListener {
+    private lateinit var listaLugares: ArrayList<Place>
     private lateinit var placeViewModel: PlaceViewModel
 
-    private lateinit var adapterLugares: AdapterLugares
-    private lateinit var recyclerViewLugares: RecyclerView
-    private lateinit var listaLugares: ArrayList<Place>
-    private lateinit var txtnombre: EditText
     //Crear referencias para poder realizar la comunicacion entre el fragment detalle
     private lateinit var actividad: Activity
     private lateinit var interfaceComunicaFragments: iComunicaFragments
@@ -38,90 +36,38 @@ class LugaresFragment : Fragment() {
     private lateinit var placeDao: PlaceDao
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        dataBase= Room.databaseBuilder(requireContext(), LabTresDB::class.java, "mi_db").allowMainThreadQueries().build()
         return inflater.inflate(R.layout.fragment_lugares, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        placeViewModel= ViewModelProvider(this).get(PlaceViewModel::class.java)
-        txtnombre = view.findViewById(R.id.txtlugar)
-        //recyclerViewLugares = view.findViewById(R.id.recyclerView)//111111
-        listaLugares = ArrayList()
-        //cargarLista()
-        //mostrarRecyclerData()
-        initRecycler()
-        observableLiveData()
+        createViewModel()
+        listaLugares=placeViewModel.cargarLista(requireContext())
+        placesListRecyclerView.layoutManager= LinearLayoutManager(requireContext())
+        placesListRecyclerView.addItemDecoration(DividerItemDecoration(requireContext(),1))
+        placesListRecyclerView.adapter= PlaceAdapter(listaLugares,this)
     }
 
-    private fun initRecycler(){
-        recyclerViewLugares = recyclerView
-        adapterLugares= AdapterLugares(requireContext(), listaLugares)
-        recyclerViewLugares.adapter=adapterLugares
-        recyclerViewLugares.layoutManager= LinearLayoutManager(requireContext())
-        adapterLugares.setOnclickListener {
-            val nombre = listaLugares[recyclerViewLugares.getChildAdapterPosition(it)].name
-            txtnombre.setText(nombre)
-            Toast.makeText(context, "Seleccionó: " + listaLugares[recyclerViewLugares.getChildAdapterPosition(it)].name, Toast.LENGTH_SHORT).show()
-            interfaceComunicaFragments.enviarLugar(listaLugares[recyclerViewLugares.getChildAdapterPosition(it)])
-        }
-
+    /**
+     * Se instancia el viewModel para poder usarlo
+     */
+    private fun createViewModel(){
+        val placeDao: PlaceDao = LabTresDB.getDatabaseInstance(requireContext()).lugarDao()
+        val repository= PlaceRepository(dataBase,placeDao)
+        val factory= PlaceViewModelFactory(repository)
+        placeViewModel= ViewModelProvider(this,factory).get(PlaceViewModel::class.java)
     }
+//    private fun observableLiveData(){
+//        placeViewModel.places.observe(viewLifecycleOwner, Observer{place ->
+//            place?.let { adapterLugares.setPlaces(it as ArrayList<Place>) }
+//        })
+//    }
 
-    private fun observableLiveData(){
-        placeViewModel.places.observe(viewLifecycleOwner, Observer{place ->
-            place?.let { adapterLugares.setPlaces(it as ArrayList<Place>) }
-        })
-    }
+    override fun onItemClick(item: Place, position: Int) {
+        Toast.makeText(requireContext(), item.name , Toast.LENGTH_SHORT).show()
+        interfaceComunicaFragments.enviarLugar(listaLugares[position])
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        //esto es necesario para establecer la comunicacion entre la lista y el detalle
-        //si el contexto que le esta llegando es una instancia de un activity:
-        if (context is Activity) {
-            //voy a decirle a mi actividad que sea igual a dicho contexto. casting correspondiente:
-            actividad = context
-            ////que la interface icomunicafragments sea igual ese contexto de la actividad:
-            interfaceComunicaFragments = actividad as iComunicaFragments
-            //esto es necesario para establecer la comunicacion entre la lista y el detalle
-        }
-    }
-
-
-
-
-    /*fun cargarLista(){
-        var nombre: String?
-        var desC: String?
-        var desL: String?
-        var img: Int
-        dataBase= Room.databaseBuilder(requireContext(), LabTresDB::class.java, "mi_db").allowMainThreadQueries().build()
-        placeDao= dataBase.lugarDao()
-        val lugares= placeDao.getPlaces()
-        val tama= placeDao.getPlaces().size
-        //Toast.makeText(context, "el tamaño es " + tama, Toast.LENGTH_SHORT).show()
-        for (i in 0 until tama){
-            nombre= lugares[i].name
-            desC= lugares[i].short_des
-            desL= lugares[i].long_desc
-            img= lugares[i].img
-            listaLugares.add(Place(0,nombre, desC, desL, img))
-        }
-    }
-
-    private fun mostrarRecyclerData() {
-        recyclerViewLugares.layoutManager = LinearLayoutManager(context)
-        adapterLugares = AdapterLugares(context, listaLugares)
-        recyclerViewLugares.adapter = adapterLugares
-        adapterLugares.setOnclickListener { view ->
-            val nombre = listaLugares[recyclerViewLugares.getChildAdapterPosition(view)].name
-            txtnombre.setText(nombre)
-            Toast.makeText(context, "Seleccionó: " + listaLugares[recyclerViewLugares.getChildAdapterPosition(view)].name, Toast.LENGTH_SHORT).show()
-            //enviar mediante la interface el objeto seleccionado al detalle
-            //se envia el objeto completo
-            //se utiliza la interface como puente para enviar el objeto seleccionado
-            interfaceComunicaFragments.enviarLugar(listaLugares[recyclerViewLugares.getChildAdapterPosition(view)])
-            //luego en el mainactivity se hace la implementacion de la interface para implementar el metodo enviarpersona
-        }
     }
 
     override fun onAttach(context: Context) {
@@ -135,6 +81,5 @@ class LugaresFragment : Fragment() {
             interfaceComunicaFragments = actividad as iComunicaFragments
             //esto es necesario para establecer la comunicacion entre la lista y el detalle
         }
-    }*/
+    }
 }
-
